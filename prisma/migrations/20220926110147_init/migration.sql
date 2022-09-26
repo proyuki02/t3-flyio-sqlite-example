1bf8137f9920786d952d94de4795f0c4a5e75db2
proyuki02 <proyuki02@gmail.com>
2022-09-26 21:13:53 +0900

    DBのデータを永続ボリュームに保存する

---
+++ prisma/migrations/20220926110147_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Example" (
+    "id" TEXT NOT NULL PRIMARY KEY
+);
+
+-- CreateTable
+CREATE TABLE "Counter" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "count" INTEGER NOT NULL
+);
